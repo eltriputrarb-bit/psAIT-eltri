e02feedff0ffceb6d7ac5addf8b1e647cfccfd89
ELTRI <eltriputrarb@gmail.com>
2026-05-18 20:57:27 +0800

    Update deteksi_wajah.py

--- deteksi_wajah.py
+++ deteksi_wajah.py
@@ -109,4 +109,4 @@ if __name__ == '__main__':
     print("\n==================================================================")
     print("--- SERVER INTELLIGENCE ANTI-SPOOFING PORT 5000 ACTIVE ---")
     print("==================================================================")
-    app.run(host='127.0.0.1', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
