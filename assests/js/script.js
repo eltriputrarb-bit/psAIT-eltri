81f3afadf03a8815bcb7c243e76720944ff194f6
ELTRI <eltriputrarb@gmail.com>
2026-05-20 21:44:18 +0800

    Delete assests directory

--- assests/js/script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const menuBtn = document.getElementById('mobile-menu');
-    const navLinks = document.getElementById('nav-links');
-
-    // Ini yang bikin garis jadi X dan menu muncul
-    menuBtn.classList.toggle('is-active'); 
-    navLinks.classList.toggle('active');
-}
